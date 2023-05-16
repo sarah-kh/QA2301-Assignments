91e226236b2393ada677ff8059d1e6c75b60841d
Sarah Kh <sarah.kh006@gmail.com>
2023-05-16 13:47:00 -0400

    new code

---
+++ JavaAssignmants/src/main/java/javaAssignmants01/Comment.java
@@ -0,0 +1,15 @@
+package javaAssignmants01;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Comment {
+    public static void main(String[] args) {
+
+        System.out.println("Name of the author: John\n" +
+                "content of the book: programming\n" +
+                "publish date: 01/01/23");
+
+
+    }
+}
---
+++ JavaAssignmants/src/main/java/javaAssignmants01/CreditCard.java
@@ -0,0 +1,10 @@
+package javaAssignmants01;
+
+public class CreditCard {
+    public static void main(String[] args) {
+        System.out.println("Hello from credit card class!\n"+
+                "Card holder`s name: John\n" +
+                "card number: 6789\n" +
+                "expiration date: 01/01/24");
+    }
+}
