13f56bfabda4158c9a9d76214282a2243ca3c2f4
Sarah Kh <sarah.kh006@gmail.com>
2023-05-16 12:57:51 -0400

    Advertisement

---
+++ JavaAssignmants/src/main/java/javaAssignmants01/Advertisement.java
@@ -0,0 +1,9 @@
+package javaAssignmants01;
+
+public class Advertisement {
+    public static void main(String[] args) {
+        System.out.println("description: " + "cell phone");
+        System.out.println("target audience: " + "18 year old and above");
+
+    }
+}
