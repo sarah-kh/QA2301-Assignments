ac430d1865ecb89f873b03fda5cb19ff6d83b37f
Sarah Kh <sarah.kh006@gmail.com>
2023-05-16 12:53:18 -0400

    Address

--- JavaAssignmants/src/main/java/javaAssignmants01/Address.java
+++ JavaAssignmants/src/main/java/javaAssignmants01/Address.java
@@ -1,4 +1,10 @@
 package javaAssignmants01;
 
 public class Address {
+    public static void main(String[] args) {
+        System.out.println("Hello from address class");
+        System.out.println ("Name: " + "Sarah");
+        System.out.println("zip: " + "10001");
+        System.out.println("city: " + " New York");
+    }
 }
